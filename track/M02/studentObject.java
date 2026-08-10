374233d08a8cd99a3ccc8f2ea4de41bf055a8008
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-10 17:53:57 +0530

    student object

---
+++ track/M02/studentObject.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+class Student {
+
+    // Declare id, name, course and javaScore
+    int id;
+    String name;
+    String course;
+    double javaScore;
+}
+
+public class studentObject {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create one Student object
+        Student student = new Student();
+
+        // Read and store all values in the object
+        student.id = scanner.nextInt();
+        student.name = scanner.next();
+        student.course = scanner.next();
+        student.javaScore = scanner.nextDouble();
+
+        // Display the values stored in the object
+        System.out.println("Student Profile");
+        System.out.println("ID: " + student.id);
+        System.out.println("Name: " + student.name);
+        System.out.println("Course: " + student.course);
+        System.out.println("Java Score: " + student.javaScore);
+    }
+}
