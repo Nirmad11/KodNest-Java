3e481d9cb70fa5cd64f7c64076624dba2cd7bd1b
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-12 16:22:45 +0530

    first method

---
+++ track/M02/firstmethod.java
@@ -0,0 +1,17 @@
+
+class Robot {
+
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class firstmethod {
+
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+        robot.speak();
+
+    }
+}
