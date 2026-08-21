2420b4191cc4b3b1b9c3f521dd66bfab995297b5
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-21 13:11:25 +0530

    read and dispaly matrix

---
+++ track/M02/matrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        // Read matrix elements
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Display matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
