6e77b5f8b9fb5d0e29c3e34aa04536c0d3891b97
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-31 10:38:10 +0530

    given array is sorted or not

--- track/M03/sorting.java
+++ track/M03/sorting.java
@@ -11,8 +11,8 @@ public class sorting {
             arr[i] = scanner.nextInt();
         }
         int sorted = 1;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < arr[i + 1]) {
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
                 sorted = 0;
                 break;
             }
