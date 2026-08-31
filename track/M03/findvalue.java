801fc9c3f0b9db147539695e8a77b920a3972543
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-31 09:57:10 +0530

    search given value

---
+++ track/M03/findvalue.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class findvalue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int target = scanner.nextInt();
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
+
+        scanner.close();
+    }
+}
