6fae73a325a91a092363984f97b8ba465c1a7d65
Nirmad11 <nirmadnirmad@gmail.com>
2026-08-31 10:06:48 +0530

    array are identical

---
+++ track/M03/arrayidentical.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class arrayidentical {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        int result = 1;
+        if (n != m) {
+            result = 0;
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (arr1[i] != arr2[i]) {
+                    result = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
