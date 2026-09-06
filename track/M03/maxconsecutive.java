c8118d33e07cbffa7187d36c7694ae8079ddaa9b
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-06 12:42:28 +0530

    find max consecutive one's

---
+++ track/M03/maxconsecutive.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+
+public class maxconsecutive {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                if (count > max) {
+                    max = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}
