506419ea85546679246cdaec1d178718d24a85b3
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-06 12:49:33 +0530

    separte even and odd

---
+++ track/M03/sepevenodd.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+import java.util.*;
+
+public class sepevenodd {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            if (arr[left] % 2 == 0) {
+                left++;
+            } else if (arr[right] % 2 != 0) {
+                right--;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+            if (i != n - 1) {
+                sb.append(" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
