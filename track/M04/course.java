8cb76f94ff5c986433cf4dd3c486c8f156b4ff33
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-14 11:23:11 +0530

    read course details

---
+++ track/M04/course.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class course {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read and display the course and trainer names.
+        String course = scanner.nextLine();
+        String trainer = scanner.nextLine();
+
+        System.out.println("Course: " + course);
+        System.out.println("Trainer: " + trainer);
+    }
+}
