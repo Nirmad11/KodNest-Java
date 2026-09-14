676b0d70bcd9895ec94de11f1fc8fcc1752c5486
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-14 11:40:54 +0530

    find word in text

---
+++ track/M04/word.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence = scanner.nextLine();
+        String word = scanner.nextLine();
+
+        // Search and display the result.
+        boolean found = sentence.contains(word);
+        System.out.println("Found: " + found);
+    }
+}
