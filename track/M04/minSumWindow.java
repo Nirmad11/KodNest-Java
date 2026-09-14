d82682af4b3b0a8fe870775bc93dc8cee88af515
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-14 10:47:25 +0530

    minimum sum of window size k

---
+++ track/M04/minSumWindow.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class minSumWindow {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLong();
+        }
+
+        int k = sc.nextInt();
+
+        // sum of the first window
+        long windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        long minSum = windowSum;
+
+        // slide the window: add the new element, remove the one that fell out
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            if (windowSum < minSum) {
+                minSum = windowSum;
+            }
+        }
+
+        System.out.println(minSum);
+    }
+}
