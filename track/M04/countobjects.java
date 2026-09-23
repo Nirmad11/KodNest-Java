f70c3360641e7e3b8fbb653bc43c21543040b79f
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-23 14:24:53 +0530

    created objects count

---
+++ track/M04/countobjects.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class Student {
+
+    static int count;
+
+    Student() {
+        // Increase the shared count.
+        count++;
+    }
+}
+
+public class countobjects {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStudents = scanner.nextInt();
+
+        // Create the required objects and display Student.count.
+        for (int i = 0; i < numberOfStudents; i++) {
+            new Student();
+        }
+
+        System.out.println("Created students: " + Student.count);
+    }
+}
