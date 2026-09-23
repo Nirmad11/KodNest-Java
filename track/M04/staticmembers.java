26d62539713bbcee3be87d80c85c478221e10f1b
Nirmad11 <nirmadnirmad@gmail.com>
2026-09-23 14:58:20 +0530

    statis and instance members

---
+++ track/M04/staticmembers.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+class Learner {
+
+    String name;
+    static String institute = "KodNest";
+
+    Learner(String name) {
+        // Store the learner name.
+        this.name = name;
+    }
+
+    void displayName() {
+        // Display the instance name.
+        System.out.println("Learner: " + name);
+    }
+
+    static void displayInstitute() {
+        // Display the static institute.
+        System.out.println("Institute: " + institute);
+    }
+}
+
+public class staticmembers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read a name and use the correct forms of access.
+        String name = scanner.nextLine();
+        Learner learner = new Learner(name);
+        learner.displayName();
+        Learner.displayInstitute();
+    }
+}
